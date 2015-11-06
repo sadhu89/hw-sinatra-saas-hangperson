df487e54025333261d7b8a5a7b58cff385ee8f09
Carlos Rojas <carlos.sadhu@gmail.com>
2015-11-06 06:21:17 +0000

    Part 01

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,10 +8,40 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses=''
+    @wrong_guesses=''
   end
 
+  def guess(letter)
+    raise ArgumentError if letter=='' || letter.nil? || (/[^a-zA-Z]/===letter)
+    letter=letter.downcase
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+    
+    if @word.include? letter
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end 
+  end
+  
+  def word_with_guesses
+    word=@word
+    word.split('').each do |l|
+      word.gsub!(l,'-') if !@guesses.include? l
+    end
+    word
+  end
+  
+  def check_win_or_lose
+    return :lose if wrong_guesses.length>=7
+    return :win if @word.split('').all?{|l| @guesses.include? l }
+    return :play
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
