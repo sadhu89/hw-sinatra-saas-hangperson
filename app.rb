9c6fded116e1a656e7761e53962dd335c9fa5273
Carlos Rojas <carlos.sadhu@gmail.com>
2015-11-07 06:09:29 +0000

    Ready to Heroku

--- app.rb
+++ app.rb
@@ -40,6 +40,11 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+    flash[:message]="You have already used that letter." if !@game.guess letter
+    rescue ArgumentError
+      flash[:message]="Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,6 +55,9 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    res=@game.check_win_or_lose
+    redirect '/win' if res==:win
+    redirect '/lose' if res==:lose
     erb :show # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -11,13 +11,13 @@ class HangpersonGame
   attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
-    @word = word.downcase
+    @word = word.downcase #hello
     @guesses=''
     @wrong_guesses=''
   end
 
-  def guess(letter)
-    raise ArgumentError if letter=='' || letter.nil? || (/[^a-zA-Z]/===letter)
+  def guess(letter)  # guess('e') => false
+    raise ArgumentError if letter.nil? || letter.empty? || (/[^a-zA-Z]/===letter)
     letter=letter.downcase
     return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
     
@@ -29,16 +29,22 @@ class HangpersonGame
   end
   
   def word_with_guesses
+    # word = hello 
+    # @guesses('eo') -> '-e--o'
+    word.gsub(/[^"#{guesses}"]/, '-')
+=begin
     word=@word
     word.split('').each do |l|
-      word.gsub!(l,'-') if !@guesses.include? l
+      word.gsub!(l,'-') if !@guesses.include? l #-e--
     end
     word
+=end
   end
   
   def check_win_or_lose
     return :lose if wrong_guesses.length>=7
-    return :win if @word.split('').all?{|l| @guesses.include? l }
+    # return :win if @word.split('').all?{|l| @guesses.include? l }
+    return :win if word == word_with_guesses
     return :play
   end
   
@@ -48,5 +54,4 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
 end
